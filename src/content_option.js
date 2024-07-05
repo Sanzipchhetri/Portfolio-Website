bae25d44bcecb5d08920f0dfebc13f5d2aa37599
RTMK <gyawali.sanjeevs98@gmail.com>
2024-07-05 17:18:10 +0545

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,171 @@
+const logotext = "SANJEEV";
+const meta = {
+    title: "Sanjeev Gyawali",
+    description: "I’m Sanjeev Gyawali data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Sanjeev Gyawali",
+    animated: {
+        first: "a Creative Digital Marketer",
+        second: "a Graphic Designer",
+        third: "I Build Branding",
+    },
+    description: "I'M EASY TO FIND DON'T BE SHY.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Know About Me",
+    aboutme: "Overall, I hold a bachelor’s degree in computer science with Honors, specializing in Information Technology. I have accumulated more than a years of hands-on experience in the digital marketing field. My educational background and practical experience uniquely position me to excel in the dynamic and evolving landscape of digital marketing.",
+};
+const worktimeline = [{
+        jobtitle: "Digital Marketing Intern",
+        where: "Gusto Infosys",
+        date: "2018",
+    },
+    {
+        jobtitle: "Digital Marketing Executive",
+        where: "Darshan Solution",
+        date: "2021-2023",
+    },
+    {
+        jobtitle: "IT Team Lead",
+        where: "Urbar Nepal",
+        date: "2021-2023",
+    },
+    {
+        jobtitle: "Digital Marketing Associate",
+        where: "Carpenters Stop",
+        date: "2023-Present",
+    },
+];
+
+const skills = [{
+        name: "Digital Marketing",
+        value: 90,
+    },
+    {
+        name: "Graphic Design",
+        value: 85,
+    },
+    {
+        name: "Web Design and Development",
+        value: 80,
+    },
+    {
+        name: "Product Design",
+        value: 60,
+    },
+    {
+        name: "Content Creation",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Digital Marketing",
+        description: "Helping businesses discover how to best use of digital Platform to connect with customers, generate awareness, and increase sales.",
+    },
+    {
+        title: "Graphic Design",
+        description: "Helping to craft a great valuable post with visually compelling graphic design that can grab the attention of your audience and increase your post’s engagement.",
+    },
+    {
+        title: "Web Design and Development",
+        description: "Creating a brand-specific and attractive website design that catches your user's attention and drive more traffic.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "info@thesanjeev.com.np",
+    YOUR_FONE: "+977-9868250518",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://www.facebook.com/sanjeev.gyawali.3",
+    linkedin: "https://www.linkedin.com/in/sanzip-chhetri-467108185/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
